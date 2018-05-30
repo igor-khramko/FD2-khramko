a1fdef6bbcae064b20856dc530de70ec95d0243b
i.khramko <igorkhramko@gmail.com>
2018-05-30 21:00:27 +0300

    N.07

---
+++ N.07/script.js
@@ -0,0 +1,14 @@
+function vowels(){
+    var str = prompt("Введите текст");
+    var vowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
+    var count = 0;
+    for(var i = 0; i<str.length; i++){
+        for(var j = 0; j < vowels.length - 1; j++){
+            if(str[i].toLowerCase() == vowels[j]){
+                count++;
+            }
+        }
+    }
+    return console.log(`Гласных букв в строке: ${count}`); 
+}
+vowels();
