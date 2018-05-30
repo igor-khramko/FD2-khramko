d78952fca8668396b79aecba928398546dc9f3cb
igor-khramko <igorkhramko@gmail.com>
2018-05-30 21:55:27 +0300

    Update script.js

--- N.07/script.js
+++ N.07/script.js
@@ -3,10 +3,8 @@ function vowels(){
     var vowels = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
     var count = 0;
     for(var i = 0; i<str.length; i++){
-        for(var j = 0; j < vowels.length - 1; j++){
-            if(str[i].toLowerCase() == vowels[j]){
-                count++;
-            }
+        for(var j = 0; j < vowels.length; j++){
+            str[i].toLowerCase() == vowels[j] ? count++ : count;
         }
     }
     return console.log(`Гласных букв в строке: ${count}`); 
