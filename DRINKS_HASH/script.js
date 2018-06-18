216e332e3c63a517fadecb5b0fef85a8f2025c5e
i.khramko <igorkhramko@gmail.com>
2018-06-18 23:20:39 +0300

    N.13 DRINKS_HASH

---
+++ DRINKS_HASH/script.js
@@ -0,0 +1,41 @@
+function HashStorage(){
+    var self = this;
+    self.storage = {};
+    self.addValue = function(key, value){
+        self.storage[key] = value;
+        return self.storage;
+    }
+    self.getValue = function(key){
+        return self.storage[key];
+    }
+    self.deleteValue = function(key){
+        if(key in self.storage){
+            delete self.storage[key];
+            return true;
+        } else{
+            return false;
+        }
+    }
+    self.getList = function(){
+        return Object.keys(self.storage);
+    }
+}
+var drinkStorage =new HashStorage();
+
+function add(){
+    var drinkName = prompt("Введите название напитка");
+    var drinkInfo = prompt("Введите тип и рецепт напитка");
+    drinkStorage.addValue(drinkName, drinkInfo);
+    console.log(drinkStorage.storage);
+}
+function get(){
+    var drinkName = prompt("Введите название напитка");
+    console.log("Информация о " + "'" + drinkName + "': " + drinkStorage.getValue(drinkName));
+}
+function deleteKey(){
+    var drinkName = prompt("Введите ключ");
+    drinkStorage.deleteValue(drinkName);
+}
+function getList(){
+    console.log(drinkStorage.getList());
+}
