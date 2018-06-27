50445a6625a739bf65ac3f8edb1e74d55d172546
i.khramko <igorkhramko@gmail.com>
2018-06-27 23:10:31 +0300

    N.10 MOOD

---
+++ N.10_MOOD/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+    function randomDiap(n,m) {
+            return Math.floor(Math.random()*(m-n+1))+n;
+    }
+
+    function mood(colorsCount) {
+        var colors=[ '', 'красный', 'оранжевый', 'жёлтый', 'зелёный', 'голубой', 'синий', 'фиолетовый' ];
+        console.log( 'цветов: ' + colorsCount );
+        var colorsHash = {};
+        for ( var i=1; i<=colorsCount; i++ ) {
+            var n=randomDiap(1,7);
+            if(colors[n] in colorsHash){
+                continue;
+            }
+            colorsHash[colors[n]] = true;
+        }
+        return colorsHash;
+    }
+    console.log(mood(3));
